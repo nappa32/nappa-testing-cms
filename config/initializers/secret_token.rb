4f03ea57d2f999c91922c1842c610f04cb22e0c4
Imran Ahmed <imran.nappa@gmail.com>
2014-09-16 10:49:52 +1200

    testing release

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NappaTestingCms::Application.config.secret_token = '606c2dd9a6c764349bbd736383d32c63160812bf8396a27fd32cd2bd3003c95c383bb44e0111904d426e76e7ef27167927ce79498192cb2d50445d90d6e6ad24'
